4c655cba26bb099f9102eef53704f8334e11d657
kadhirvelsubramanian <164773976+kadhirvelsubramanian@users.noreply.github.com>
2025-07-30 22:37:16 +0530

    Remove config folder from tracking

--- server/config/connectDatabase.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URL).then((con)=>{
-        console.log("MongoDB connected to  : "+con.connection.host);
-    })
-};
-module.exports=connectDatabase;
