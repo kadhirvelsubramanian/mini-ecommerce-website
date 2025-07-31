97c6567cb49e844bc121b7952e6341f16720c871
kadhirvelsubramanian <164773976+kadhirvelsubramanian@users.noreply.github.com>
2025-07-31 11:49:37 +0530

    temp commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,30 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './pages/home';
 import ProductDetail from './pages/productDetail';
-import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState } from "react"
-import {ToastContainer} from 'react-toastify'
+import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import Cart from './pages/cart';
 
 function App() {
-  const [cartItems,setCartItems]=useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   return (
     <div className="App">
       <Router>
         <div>
-          <ToastContainer theme='dark' position='top-center'/>
-        <Header cartItems={cartItems}/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/search" element={<Home/>}/>
-          <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems}/>}/>
-          <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems}/>}/>
-        </Routes>
+          <ToastContainer theme='dark' position='top-center' />
+          <Header cartItems={cartItems} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail cartItems={cartItems} setCartItems={setCartItems} />} />
+            <Route path="/cart" element={<Cart cartItems={cartItems} setCartItems={setCartItems} />} />
+          </Routes>
         </div>
       </Router>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
