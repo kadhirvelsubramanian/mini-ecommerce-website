7519d6efe867594c09864a56852ab87645e2c1ae
kadhirvelsubramanian <164773976+kadhirvelsubramanian@users.noreply.github.com>
2025-08-02 10:58:05 +0530

    Error fix in home.js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,33 +1,32 @@
 import React from "react";
-import { Fragment,useState,useEffect } from "react";
+import { Fragment, useState, useEffect } from "react";
 import ProductCard from "../components/ProductCard";
 import { useSearchParams } from "react-router-dom";
 
-export default function Home()
-{   
-    const [products,setProducts]= useState([]);
-    const [searchParams,setSearchParams]=useSearchParams();
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-    useEffect(()=>{
-        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
-        .then(res=>res.json())
-        .then(res=>setProducts(res.products));
-    },[searchParams])
+  useEffect(() => {
+    fetch(process.env.REACT_APP_API_URL + '/products' + searchParams)
+      .then(res => res.json())
+      .then(res => setProducts(res.products));
+  }, [searchParams])
 
-    //     useEffect(()=>{
-    //     fetch(process.env.REACT_APP_API_URL+'/products?')
-    //     .then(res=>res.json())
-    //     .then(res=>setProducts(res.products));
-    // },[])
+  //     useEffect(()=>{
+  //     fetch(process.env.REACT_APP_API_URL+'/products?')
+  //     .then(res=>res.json())
+  //     .then(res=>setProducts(res.products));
+  // },[])
 
-    return <Fragment>
+  return <Fragment>
     <h1 id="products_heading">Latest Products</h1>
 
     <section id="products" className="container mt-5">
       <div className="row">
-        {products.map(product=><ProductCard key={product._id} product={product}/>)}
+        {products.map(product => <ProductCard key={product._id} product={product} />)}
       </div>
     </section>
 
-    </Fragment>
+  </Fragment>
 }
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,7 +1,7 @@
-const express=require('express');
+const express = require('express');
 const { createOrder } = require('../controller/orderController');
-const router=express.Router();
+const router = express.Router();
 
 router.route('/order').post(createOrder)
 
-module.exports=router;
+module.exports = router;
